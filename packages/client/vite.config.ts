b2224d9322cc54e66c4b61179581a435f5945c9d
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2025-11-08 15:18:28 +0300

    [eslintrc-prettierrc-set-settings] Доработка конфига для корректного запуска

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -8,28 +8,26 @@ import { defineConfig } from 'vite';
 dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    server: {
-        port: Number(process.env.CLIENT_PORT) || 3000,
-    },
-    define: {
-        __EXTERNAL_SERVER_URL__: JSON.stringify(
-            process.env.EXTERNAL_SERVER_URL
-        ),
-        __INTERNAL_SERVER_URL__: JSON.stringify(
-            process.env.INTERNAL_SERVER_URL
-        ),
-    },
-    build: {
-        outDir: path.join(__dirname, 'dist/client'),
-    },
-    ssr: {
-        format: 'cjs',
-    },
-    plugins: [react(), tsconfigPaths()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+export default defineConfig(async () => {
+    const tsconfigPaths = (await import('vite-tsconfig-paths')).default;
+    return {
+        server: {
+            port: Number(process.env.CLIENT_PORT) || 3000,
         },
-    },
+        define: {
+            __EXTERNAL_SERVER_URL__: JSON.stringify(
+                process.env.EXTERNAL_SERVER_URL
+            ),
+            __INTERNAL_SERVER_URL__: JSON.stringify(
+                process.env.INTERNAL_SERVER_URL
+            ),
+        },
+        build: {
+            outDir: path.join(__dirname, 'dist/client'),
+        },
+        ssr: {
+            format: 'cjs',
+        },
+        plugins: [react(), tsconfigPaths()],
+    };
 });
