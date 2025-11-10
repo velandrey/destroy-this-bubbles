83a3acec196bc5de286de40aacbcbf54d25ad5ba
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2025-11-10 10:05:50 +0300

    [add-mui-library] Правка стилей, формирования объекта стилей

---
+++ packages/client/src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const darkTheme = createTheme({
+    palette: {
+        mode: 'light',
+        // TODO all: попробовать реализовать темную тему
+        // background: {
+        //     default: '#222222',
+        //     paper: '#333333',
+        // },
+    },
+});
