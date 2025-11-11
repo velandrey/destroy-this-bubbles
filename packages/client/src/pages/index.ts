0106119f593e5d6d119b0ffc51d5baacd32cbd40
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2025-11-11 16:06:11 +0300

    [sprint_5-add_routing] Добавление остальных страниц и редиректа

--- packages/client/src/pages/index.ts
+++ packages/client/src/pages/index.ts
@@ -1,4 +1,19 @@
 import { LoginPage } from './login';
 import { MenuPage } from './menu';
+import { RegistrationPage } from './registration';
+import { ProfilePage } from './profile';
+import { GamePage } from './game';
+import { LeaderBoardPage } from './leaderBoard';
+import { ForumPage } from './forum';
+import { ErrorPage } from './error';
 
-export { LoginPage, MenuPage };
+export {
+    LoginPage,
+    MenuPage,
+    RegistrationPage,
+    ProfilePage,
+    GamePage,
+    LeaderBoardPage,
+    ForumPage,
+    ErrorPage,
+};
--- packages/client/src/pages/menu/menu.tsx
+++ packages/client/src/pages/menu/menu.tsx
@@ -2,12 +2,48 @@ import { Page } from '@components/page';
 import { Button } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import styles from './styles.module.scss';
 
 const MenuPage = () => {
     const navigate = useNavigate();
 
     return (
-        <Page>
+        <Page className={styles.container}>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => navigate('/game')}
+            >
+                Игра
+            </Button>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => navigate('/profile')}
+            >
+                Профиль
+            </Button>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => navigate('/leaderBoard')}
+            >
+                Таблица лидеров
+            </Button>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => navigate('/forum')}
+            >
+                Форум
+            </Button>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => navigate('/error')}
+            >
+                Ошибка
+            </Button>
             <Button
                 variant="contained"
                 color="primary"
