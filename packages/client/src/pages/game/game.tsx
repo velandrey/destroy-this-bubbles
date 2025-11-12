6538757f6be4c2a401e23c07cb25bc52ca998b7f
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2025-11-12 16:25:14 +0300

    [sprint_5-game_start_screen] Добавление логики на клик

--- packages/client/src/pages/game/game.tsx
+++ packages/client/src/pages/game/game.tsx
@@ -1,14 +1,12 @@
 import { Page } from '@components/page';
 import { accordionActionsClasses, Button } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import styles from './styles.module.scss';
 
 const descriptionPart = {
     name: 'Destroy This Bubbles',
-    description:
-        'Уничтожай все пузыри, которые только можешь заметить! Делай это как можно быстрее и точнее, тогда ты станешь самым ловким стрелком на Диком Западе!',
     rules: [
         'Щёлкните по пузырям, чтобы уничтожить их',
         'Чем больше пузырей уничтожаете, тем больше очков вы получаете',
@@ -44,34 +42,85 @@ const lastTryPart = {
 const GamePage = () => {
     const navigate = useNavigate();
 
+    const [countdown, setCountdown] = useState(3);
+    const [gameState, setGameState] = useState<
+        'idle' | 'countdown' | 'playing'
+    >('idle');
+
+    useEffect(() => {
+        if (gameState === 'countdown') {
+            const timer = setInterval(() => {
+                setCountdown((prevCountdown) => prevCountdown - 1);
+            }, 1000);
+
+            if (countdown === 0) {
+                clearInterval(timer);
+                setGameState('playing');
+            }
+
+            return () => clearInterval(timer);
+        }
+    }, [gameState, countdown]);
+
+    const handleGameStart = () => {
+        setGameState('countdown');
+    };
+
     return (
         <Page className={styles.container}>
-            <div className={styles.descriptionPart}>
-                <p>{descriptionPart.description}</p>
-                <ul className={styles.list}>
-                    {descriptionPart.rules.map((rule) => (
-                        <li key={rule}>{rule}</li>
-                    ))}
-                </ul>
-                <Button variant="outlined" onClick={() => navigate('/menu')}>
-                    Назад к меню
-                </Button>
-            </div>
-            <div className={styles.startPart}>
-                <div className={styles.gameTitle}>{descriptionPart.name}</div>
-            </div>
-            <div className={styles.lastTryPart}>
-                <p className={styles.title}>Предыдущие игры</p>
-                {lastTryPart.lastGamesScore.map((game) => (
-                    <div key={game.date}>
-                        <p className={styles.date}>{game.date}</p>
-                        <p className={styles.info}>Очки: {game.score}</p>
-                        <p className={styles.info}>
-                            Точность: {game.accuracy}%
-                        </p>
+            {gameState === 'idle' && (
+                <>
+                    <div className={styles.descriptionPart}>
+                        <ul className={styles.list}>
+                            {descriptionPart.rules.map((rule) => (
+                                <li key={rule}>{rule}</li>
+                            ))}
+                        </ul>
+                        <Button
+                            variant="outlined"
+                            onClick={() => navigate('/menu')}
+                        >
+                            Назад к меню
+                        </Button>
                     </div>
-                ))}
-            </div>
+                    <div className={styles.startPart}>
+                        <div
+                            className={styles.gameTitle}
+                            onClick={handleGameStart}
+                        >
+                            {descriptionPart.name}
+                        </div>
+                    </div>
+                    <div className={styles.lastTryPart}>
+                        <h2 className={styles.title}>Предыдущие игры</h2>
+                        {lastTryPart.lastGamesScore.map((game) => (
+                            <div key={game.date}>
+                                <p className={styles.date}>{game.date}</p>
+                                <p className={styles.info}>
+                                    Очки: {game.score}
+                                </p>
+                                <p className={styles.info}>
+                                    Точность: {game.accuracy}%
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
+
+            {gameState === 'countdown' && <div>{countdown}</div>}
+
+            {gameState === 'playing' && (
+                <div>
+                    <Button
+                        color="success"
+                        variant="contained"
+                        onClick={() => navigate('/menu')}
+                    >
+                        Назад к меню
+                    </Button>
+                </div>
+            )}
         </Page>
     );
 };
