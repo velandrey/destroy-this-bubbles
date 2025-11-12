d9767dc578bdb6633d24e894aa139cb6d343b386
Alex <bolonin.lex@yandex.ru>
2025-11-12 16:41:05 +0300

    [26-login-page] Сверстана страница авторизации

--- packages/client/src/pages/login/login.tsx
+++ packages/client/src/pages/login/login.tsx
@@ -1,20 +1,43 @@
+import { Form } from '@components/form';
+import { TInput } from '@components/form/form';
 import { Page } from '@components/page';
-import { Button } from '@mui/material';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Grid } from '@mui/material';
 
 const LoginPage = () => {
-    const navigate = useNavigate();
+    const inputs: TInput[] = [
+        {
+            inputName: 'name',
+            inputLabel: 'Имя',
+            placeholder: 'Введите имя',
+        },
+        {
+            inputName: 'email',
+            inputLabel: 'Email',
+        },
+    ];
 
+    const handleSubmit = (data: Record<string, string | File | null>) => {
+        console.log(data);
+    };
     return (
         <Page>
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={() => navigate('/')}
+            <Grid
+                container
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    minHeight: '100vh',
+                }}
             >
-                Вход
-            </Button>
+                <Form
+                    submitBtnLabel="Войти"
+                    resetBtnLabel="Отменить"
+                    inputs={inputs}
+                    onSubmit={handleSubmit}
+                />
+            </Grid>
         </Page>
     );
 };
