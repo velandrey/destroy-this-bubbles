8a77002acc2338b9b2c96796e0a63ef1e454ab5a
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2025-11-12 15:07:31 +0300

    [sprint_5-game_start_screen] Добавление каркаса для страницы

--- packages/client/src/pages/game/game.tsx
+++ packages/client/src/pages/game/game.tsx
@@ -1,20 +1,77 @@
 import { Page } from '@components/page';
-import { Button } from '@mui/material';
+import { accordionActionsClasses, Button } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import styles from './styles.module.scss';
+
+const descriptionPart = {
+    name: 'Destroy This Bubbles',
+    description:
+        'Уничтожай все пузыри, которые только можешь заметить! Делай это как можно быстрее и точнее, тогда ты станешь самым ловким стрелком на Диком Западе!',
+    rules: [
+        'Щёлкните по пузырям, чтобы уничтожить их',
+        'Чем больше пузырей уничтожаете, тем больше очков вы получаете',
+        'За пропущенные пузыри вы теряете очки',
+    ],
+};
+
+const lastTryPart = {
+    lastGamesScore: [
+        {
+            date: '12.11.2025 12:00',
+            score: 100,
+            accuracy: 100,
+        },
+        {
+            date: '11.11.2025 12:00',
+            score: 76,
+            accuracy: 95,
+        },
+        {
+            date: '10.11.2025 12:00',
+            score: 50,
+            accuracy: 80,
+        },
+        {
+            date: '09.11.2025 12:00',
+            score: 25,
+            accuracy: 70,
+        },
+    ],
+};
 
 const GamePage = () => {
     const navigate = useNavigate();
 
     return (
-        <Page>
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={() => navigate('/menu')}
-            >
-                Назад
-            </Button>
+        <Page className={styles.container}>
+            <div className={styles.descriptionPart}>
+                <p>{descriptionPart.description}</p>
+                <ul>
+                    {descriptionPart.rules.map((rule) => (
+                        <li key={rule}>{rule}</li>
+                    ))}
+                </ul>
+                <Button variant="outlined" onClick={() => navigate('/menu')}>
+                    Назад к меню
+                </Button>
+            </div>
+            <div className={styles.startPart}>
+                <div className={styles.gameTitle}>{descriptionPart.name}</div>
+            </div>
+            <div className={styles.lastTryPart}>
+                <p className={styles.title}>Предыдущие игры</p>
+                {lastTryPart.lastGamesScore.map((game) => (
+                    <div key={game.date}>
+                        <p className={styles.date}>{game.date}</p>
+                        <p className={styles.info}>Очки: {game.score}</p>
+                        <p className={styles.info}>
+                            Точность: {game.accuracy}%
+                        </p>
+                    </div>
+                ))}
+            </div>
         </Page>
     );
 };
