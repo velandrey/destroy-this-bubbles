fe13e86eefcb53440ace5b7ce88e16329b69b877
Andrey Velikanov <velandrey@yandex.ru>
2025-11-23 19:27:35 +1000

    62 - Добавить Redux-хранилище Fix Test in provider

--- packages/client/src/app.test.tsx
+++ packages/client/src/app.test.tsx
@@ -1,4 +1,6 @@
+import { store } from '@store/index';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 
 import App from './App';
@@ -12,9 +14,11 @@ global.fetch = jest.fn(() =>
 
 test('Example test', async () => {
     render(
-        <MemoryRouter>
-            <App />
-        </MemoryRouter>
+        <Provider store={store}>
+            <MemoryRouter>
+                <App />
+            </MemoryRouter>
+        </Provider>
     );
     expect(screen.getByText(appContent)).toBeDefined();
 });
