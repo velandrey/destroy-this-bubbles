abab770fc8802312db6285cd281e76715b51207b
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2025-11-25 17:21:46 +0300

    [sprint_6-add_service_workers] добавление базовой реализации Service Worker

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -19,3 +19,19 @@ ReactDOM.hydrateRoot(
     </BrowserRouter>
     // </Provider>
 );
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then((registration) => {
+                console.log(
+                    'ServiceWorker registration successful with scope: ',
+                    registration.scope
+                );
+            })
+            .catch((error: string) => {
+                console.log('ServiceWorker registration failed: ', error);
+            });
+    });
+}
